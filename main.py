c2caf18ebb3ce25c193ac634350294ee74b2b996
13069807232 <2581775404@qq.com>
2026-03-07 22:52:36 +0800

    换行

--- main.py
+++ main.py
@@ -58,13 +58,13 @@ class MyPlugin(Star):
 
         chain = [
             Comp.Image.fromFileSystem(temp_file_path),
-            Comp.Plain(f"motd:{api['data']['motd']}\n"),
-            Comp.Plain(f" 玩家人数:{api['data']['p']}\{api['data']['mp']}\n"),
-            Comp.Plain(f" 今日查询最高在线:{api['data']['today_max']}\n"),
-            Comp.Plain(f" 今日查询最低在线:{api['data']['today_min']}\n"),
-            Comp.Plain(f" 历史查询最高在线:{api['data']['history_max']}\n"),
-            Comp.Plain(f" 总查询次数:{api['data']['total_queries']}\n"),
-            Comp.Plain(f" 网络延迟:{api['data']['ping']}")
+            Comp.Plain(f"\nmotd:{api['data']['motd']}"),
+            Comp.Plain(f"\n玩家人数:{api['data']['p']}\{api['data']['mp']}"),
+            Comp.Plain(f"\n今日查询最高在线:{api['data']['today_max']}"),
+            Comp.Plain(f"\n今日查询最低在线:{api['data']['today_min']}"),
+            Comp.Plain(f"\n历史查询最高在线:{api['data']['history_max']}"),
+            Comp.Plain(f"\n总查询次数:{api['data']['total_queries']}"),
+            Comp.Plain(f"\n网络延迟:{api['data']['ping']}")
         ]
         yield event.chain_result(chain)
 
